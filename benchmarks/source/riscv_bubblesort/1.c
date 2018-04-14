75e91e38aed5296e765c09dbb7e7249556470fef
Z-Gu <z_gu@outlook.com>
2018-04-14 17:09:32 +0000

    Add bubble sort test for riscv
    This patch adds a sample bubble sort program that
    can be compiled with the riscv tool chain and tested
    with sunflower-riscv.
    
    It is currently not possiible to print output, so it
    is necessary to use debuggers like gdb or the internal
    dumpmem instruction, if you know which memory region to
    watch over.

---
+++ benchmarks/source/riscv_bubblesort/1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[8];
+    a[0] = 563;
+    a[1] = 892;
+    a[2] = 8910;
+    a[3] = 76711;
+    a[4] = 61;
+    a[5] = 1523;
+    a[6] = 901;
+    a[7] = 162;
+
+    int tmp = 0;
+
+    for(int i = 0; i < 7; ++i)
+    {
+        for(int j = 0; j+i < 7; ++j)
+        {
+            if(a[j] > a[j+1])
+            {
+                tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+            }
+        }
+    }
+
+    return 0;
+}
