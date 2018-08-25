34e7f1012b5ec6e0571ed57a5a82551d8fc88102
Phillip Stanley-Marbell <phillipstanleymarbell@users.noreply.github.com>
2018-08-25 20:42:11 +0100

    Update downloads.sh

--- tools/source/downloads.sh
+++ tools/source/downloads.sh
@@ -10,16 +10,14 @@ NEWLIB_VER="newlib-1.9.0"
 
 GCC_TAR=${GCC_VER}.tar.gz
 BINUTILS_TAR=${BINUTILS_VER}.tar.gz
-NEWLIB_TAR=${NEWLIB_VER}.tar.gz"
+NEWLIB_TAR=${NEWLIB_VER}.tar.gz
 
 wget	ftp://ftp.gnu.org/pub/gnu/gcc/$GCC_VER/$GCC_TAR
 wget	ftp://ftp.gnu.org/pub/gnu/binutils/$BINUTILS_TAR
 wget	ftp://sources.redhat.com/pub/newlib/$NEWLIB_TAR
 
 tar -xvf $GCC_TAR
-
 tar -xvf $BINUTILS_TAR
-
 tar -xzvf $NEWLIB_TAR
 
 rm $NEWLIB_TAR $GCC_TAR $BINUTILS_TAR
