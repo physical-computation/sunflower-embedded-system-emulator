edd5885296f882b83cccc5e3542a197125c1f7a2
Harry Sarson <harry.sarson@hotmail.co.uk>
2019-05-15 21:10:00 +0100

    Have nexton_rapson print result and tidy up.

--- benchmarks/source/riscv_newton-raphson/newton_rapson.c
+++ benchmarks/source/riscv_newton-raphson/newton_rapson.c
@@ -1,6 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <printf.h>
 
 float f(float x)
 {
@@ -17,13 +19,15 @@ int main()
 	float x = 0.0;
 	float x_new = x - f(x)/df(x);
 	float diff = x_new - x;
-	
-	
+
+
 	while(fabs(diff) > 0.0001){
 		x = x_new;
 		x_new = x - f(x)/df(x);
 		diff = x_new - x;
 	}
-	
+
+	printf_("x^2 + 2x - 6 has a root at %f\n", x);
+
 	return 0;
 }
