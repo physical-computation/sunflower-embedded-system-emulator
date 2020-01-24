b5cf4cf199776d41ec9af85a3096a9ba1bb41d48
billtsou <btsouts@gmail.com>
2020-01-24 15:51:05 +0000

    Reverted hello.c for PR.

--- benchmarks/source/HelloWorld/hello.c
+++ benchmarks/source/HelloWorld/hello.c
@@ -2,22 +2,14 @@
 /*
  *	Uncomment for RISC-V build
  */
-
-#include <printf.h>
+/*
+	#include <printf.h>
+*/
 
 int
 main(void)
 {
-	float sumf = 0.0;
-	int sum = 0;
-
-	for (int i=0; i<10; i++)
-	{
-		sum += i;
-		sumf += i / 10.0;
-	}	
-
-	printf("Hello, World sum=%d sumf=%f.\n", sum, sumf);
+	printf("Hello, World.\n");
 
 	return 0;
 }
