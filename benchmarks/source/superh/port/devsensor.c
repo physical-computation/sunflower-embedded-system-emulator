e3b4b8d958c11e5538698834bba2bb76c0a9e1a2
Phillip Stanley-Marbell <phillipstanleymarbell@users.noreply.github.com>
2020-10-13 10:27:51 +0100

    Update devsensor.c

--- benchmarks/source/superh/port/devsensor.c
+++ benchmarks/source/superh/port/devsensor.c
@@ -3,11 +3,22 @@
 #include "devsim7708.h"
 #include "sh7708.h"
 
-float
+double
 devsignal_read(int which)
 {
-	/*	Assumes simulator always formats read as same endiannness as modeled CPU	*/
-	return *(SENSOR_READ);
+        double  reading = 0.0;
+        ulong   sensor_shift_offset = (which & 0xFFF) << 4;
+
+        ((uchar *)&reading)[7] = *(SENSOR_READ+sensor_shift_offset+0);
+        ((uchar *)&reading)[6] = *(SENSOR_READ+sensor_shift_offset+1);
+        ((uchar *)&reading)[5] = *(SENSOR_READ+sensor_shift_offset+2);
+        ((uchar *)&reading)[4] = *(SENSOR_READ+sensor_shift_offset+3);
+        ((uchar *)&reading)[3] = *(SENSOR_READ+sensor_shift_offset+4);
+        ((uchar *)&reading)[2] = *(SENSOR_READ+sensor_shift_offset+5);
+        ((uchar *)&reading)[1] = *(SENSOR_READ+sensor_shift_offset+6);
+        ((uchar *)&reading)[0] = *(SENSOR_READ+sensor_shift_offset+7);
+
+        return reading;
 }
 
 void
