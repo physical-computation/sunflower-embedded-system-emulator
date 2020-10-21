f3fae2a0cce16a93c1cf8daee2ed9f2e8911577f
JamesTimothyMeech <james.t.meech@gmail.com>
2020-10-21 16:16:48 +0100

    Clarified comments in run.m

--- benchmarks/source/superh/pedometerExample/run.m
+++ benchmarks/source/superh/pedometerExample/run.m
@@ -5,7 +5,7 @@ v
 --
 setquantum		1
 setfreq 40
---   	All signals have sample rate 400 to simulate the 400 Hz acceleromter used to collect them, interpolation and we aren't using location data
+--   	All signals have sample rate 400 to simulate the 400 Hz acceleromter used to collect them, interpolation is off and we aren't using location data
 --	Tell sunflower where to find x-axis acceleration readings
 sigsrc 			0 "X-Axis Acceleration"	0.0 0.0 	1.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0		0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0		" "	0 0.0 0.0 0.0 0	"xAcceleration.txt" 400 1 0 0
 --	Tell sunflower where to find y-axis acceleration readings
