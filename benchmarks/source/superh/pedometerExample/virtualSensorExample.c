2663c484703bc430c78d5c5c15940e9e14ed5e48
JamesTimothyMeech <james.t.meech@gmail.com>
2020-10-23 11:30:39 +0100

    switched to camelCase

--- benchmarks/source/superh/pedometerExample/virtualSensorExample.c
+++ benchmarks/source/superh/pedometerExample/virtualSensorExample.c
@@ -20,14 +20,14 @@
 #include "misc.h"
 #include "print.h"
 
-enum samples {BUFFER_SIZE = 400};
+enum samples {numberOfSamples = 400};
 struct 
-accel_axis
+accelAxis
 {
   	/*
 	 *	Hold data and info about single acceleration axis:
 	 */  
-	float 	data[BUFFER_SIZE];  /*
+	float 	data[numberOfSamples];  /*
 				     *	array to store filtered data
 				     */
 	float	max;    /*
@@ -37,7 +37,7 @@ accel_axis
   	float 	min;    /*
 			 *	minimum data entry
 			 */  
-  	float 	p2p;    /*
+  	float 	peakToPeak;    /*
 			 *	peak to peak value
 			 */	
   	float 	thresh; /*
@@ -46,17 +46,17 @@ accel_axis
 };
 
 void 
-findAxisProperties(struct accel_axis *axis)
+findAxisProperties(struct accelAxis *axis)
 {
   	/*
 	 * Find peak-to-peak value and threshold for single-axis data
 	 */
         float 	max = axis->data[0];
         float	min = axis->data[0];
-        float	p2p;
+        float	peakToPeak;
         float	thresh;
 
-        for (int i=0; i<BUFFER_SIZE; i++)
+        for (int i=0; i<numberOfSamples; i++)
 	{
         	if (axis->data[i] > max)
 		{
@@ -68,7 +68,7 @@ findAxisProperties(struct accel_axis *axis)
             	}
         }
 
-        p2p = max-min;
+        peakToPeak = max-min;
         thresh = (max+min)/2.0;
 
         /*
@@ -76,47 +76,47 @@ findAxisProperties(struct accel_axis *axis)
 	 */	
         axis->max = max;
         axis->min = min;
-        axis->p2p = p2p;
+        axis->peakToPeak = peakToPeak;
         axis->thresh = thresh;
 }
 
 int 
-chooseAxis(struct accel_axis *x, struct accel_axis *y, struct accel_axis *z, float calib)
+chooseAxis(struct accelAxis *x, struct accelAxis *y, struct accelAxis *z, float calib)
 {
         /*
 	 *	Perform maximum activity axis selection
 	 */	
 
-        float	p2p[3];
-        float	max_p2p = 0;
-        int	max_index;
+        float	peakToPeak[3];
+        float	maxPeakToPeak = 0;
+        int	maxIndex;
 
         findAxisProperties(x);
         findAxisProperties(y);
         findAxisProperties(z);
 
-        p2p[0] = x->p2p;
-        p2p[1] = y->p2p;
-        p2p[2] = z->p2p;
+        peakToPeak[0] = x->peakToPeak;
+        peakToPeak[1] = y->peakToPeak;
+        peakToPeak[2] = z->peakToPeak;
 
         /*
 	 *	Find axis with greatest peak-to-peak (p2p) amplitude
 	 */	
         for (int i = 0; i < 3; i++)
 	{
-        	if (p2p[i] > max_p2p)
+        	if (peakToPeak[i] > maxPeakToPeak)
 		{
-              		max_p2p = p2p[i];
-              		max_index = i;
+              		maxPeakToPeak = peakToPeak[i];
+              		maxIndex = i;
           	}
         }
 
         /*
 	 *	If p2p value of chosen axis is above amplitude calibration value then return chosen axis
 	 */	
-        if (max_p2p > calib) 
+        if (maxPeakToPeak > calib) 
 	{
-		return (max_index + 1);
+		return (maxIndex + 1);
         }
 
         /*
@@ -130,7 +130,7 @@ chooseAxis(struct accel_axis *x, struct accel_axis *y, struct accel_axis *z, flo
 }
 
 float 
-detectSteps(struct accel_axis *chosen)
+detectSteps(struct accelAxis *chosen)
 {
 	/*	
 	 *	Finds where threshold is crossed in negative slope direction
@@ -138,7 +138,7 @@ detectSteps(struct accel_axis *chosen)
 
     	float	steps = 0;
     	float	current, next;
-    	for(int i=0; i<BUFFER_SIZE-1;i++)
+    	for(int i=0; i<numberOfSamples-1;i++)
 	{
         	current = chosen->data[i];
         	next = chosen->data[i+1];
@@ -151,9 +151,9 @@ detectSteps(struct accel_axis *chosen)
 }
 
 void 
-readRawData(float *t_buffer, float *x_buffer, float *y_buffer, float *z_buffer)
+readRawData(float *tBuffer, float *xBuffer, float *yBuffer, float *zBuffer)
 {
-	for(int j = 0; j < BUFFER_SIZE; j++)
+	for(int j = 0; j < numberOfSamples; j++)
     	{
 		/* 
 		 * Insert delay of 2500 uSeconds to simulate 400 Hz sample rate
@@ -162,16 +162,17 @@ readRawData(float *t_buffer, float *x_buffer, float *y_buffer, float *z_buffer)
 		/* 
 		 * Read sensor readings from sigsrc 0 which is the x-axis accelerometer readings
 		 */
-    		x_buffer[j] = devsignal_read(0);
+    		xBuffer[j] = devsignal_read(0);
 		/* 
 		 * Read sensor readings from sigsrc 1 which is the y-axis accelerometer readings
 		 */
-    		y_buffer[j] = devsignal_read(1);
+    		yBuffer[j] = devsignal_read(1);
 		/* 
 		 * Read sensor readings from sigsrc 2 which is the z-axis accelerometer readings
 		 */ 
-    		z_buffer[j] = devsignal_read(2);
-		/*	Please note that sigsrc simulates a real signal which changes in time. 
+    		zBuffer[j] = devsignal_read(2);
+		/*	
+		 *	Please note that sigsrc simulates a real signal which changes in time. 
 		 *	The value returned by devsignal_read() will be different at different simulation times.
 	   	 *	Here we use delay to wait for 1/f seconds where f is the 400 Hz sample frequency specified in the run.m file. 
 	   	 *	If we don't wait for this amount of time and instead sample as quickly as we can we will see the same sensor value many times. 
@@ -182,7 +183,7 @@ readRawData(float *t_buffer, float *x_buffer, float *y_buffer, float *z_buffer)
 }
 
 void 
-MovingAvgFilter(float input[], struct accel_axis *output)
+MovingAvgFilter(float input[], struct accelAxis *output)
 {
 	/*
 	 *	Implement moving average low pass filtering
@@ -194,13 +195,13 @@ MovingAvgFilter(float input[], struct accel_axis *output)
         /*	
 	 *	calculate mean of input array
 	 */	
-        for (int i=0; i<BUFFER_SIZE; i++) 
+        for (int i=0; i<numberOfSamples; i++) 
 	{
         	sum += input[i];
         }
-        mean = sum/BUFFER_SIZE;
+        mean = sum/numberOfSamples;
 
-        for (int i=0; i<BUFFER_SIZE; i++) 
+        for (int i=0; i<numberOfSamples; i++) 
 	{
         	/*
 		 *	Find mean-subtracted input
@@ -212,7 +213,7 @@ MovingAvgFilter(float input[], struct accel_axis *output)
            	output->data[i] = 0;
         }
 
-        for (int i=0; i<BUFFER_SIZE; i++) 
+        for (int i=0; i<numberOfSamples; i++) 
 	{
         	output->data[i] = input[i];
         }
@@ -222,41 +223,35 @@ MovingAvgFilter(float input[], struct accel_axis *output)
 int
 main(void)
 {
-	int	chosen_axis;
+	int	chosenAxis;
 	float	steps;
-	float	steps_total = 0;
-	float	calib_max;		/*
-					 *	used in calibration stage
-					 */	
-	bool	use_butterworth;	/*
-					 *	if true use Butterworth filter, else use Moving Average filter
-					 */	    
+	float	totalSteps = 0;
+		
+	/*
+	 *	used in calibration stage
+	 */	
+	float	calibrationMax;	
 	
 	/* 
 	 *	Declare buffers to hold raw unfiltered acceleration data
 	 */	
-    	float	t_buffer[BUFFER_SIZE];
-    	float	x_buffer[BUFFER_SIZE];
-    	float	y_buffer[BUFFER_SIZE];
-    	float	z_buffer[BUFFER_SIZE];
+    	float	tBuffer[numberOfSamples];
+    	float	xBuffer[numberOfSamples];
+    	float	yBuffer[numberOfSamples];
+    	float	zBuffer[numberOfSamples];
 
 	/*
 	 *	Declare struct for each acceleration axis (for use after filtering)
 	 */	
-    	struct	accel_axis x_accel;
-    	struct	accel_axis y_accel;
-    	struct	accel_axis z_accel;
-
-	/*
-	 *	Select type of filtering
-	 */
-    	use_butterworth = false;
+    	struct	accelAxis xAcceleration;
+    	struct	accelAxis yAcceleration;
+    	struct	accelAxis zAcceleration;
 
 	/*
 	 *	empirically calculated minimum allowable calibration value
     	 *	in units of 0.25 mg
 	 */	
-    	calib_max = 1;
+    	calibrationMax = 1;
 
 	/* 
 	 *	Stage 2: Step Detection
@@ -265,44 +260,44 @@ main(void)
     	/*
 	 *	Read raw tri-axial accelerometer data and store in buffers
 	 */	
-    	readRawData(t_buffer, x_buffer, y_buffer, z_buffer);
+    	readRawData(tBuffer, xBuffer, yBuffer, zBuffer);
 
-	MovingAvgFilter(x_buffer, &x_accel);
-    	MovingAvgFilter(y_buffer, &y_accel);
-    	MovingAvgFilter(z_buffer, &z_accel);
+	MovingAvgFilter(xBuffer, &xAcceleration);
+    	MovingAvgFilter(yBuffer, &yAcceleration);
+    	MovingAvgFilter(zBuffer, &zAcceleration);
 
     	/*
 	 *	Peform maximal activity axis selection
 	 */	
-    	chosen_axis = chooseAxis(&x_accel, &y_accel, &z_accel, calib_max);
-    	if(chosen_axis == 1)
+    	chosenAxis = chooseAxis(&xAcceleration, &yAcceleration, &zAcceleration, calibrationMax);
+    	if(chosenAxis == 1)
 	{
-        	steps = detectSteps(&x_accel);
+        	steps = detectSteps(&xAcceleration);
         }
 
-     	else if(chosen_axis == 2)
+     	else if(chosenAxis == 2)
 	{
-        	steps = detectSteps(&y_accel);
+        	steps = detectSteps(&yAcceleration);
         }
 
-     	else if(chosen_axis == 3)
+     	else if(chosenAxis == 3)
 	{
-        	steps = detectSteps(&z_accel);
+        	steps = detectSteps(&zAcceleration);
         }
 
      	else 
 	{
         	steps = 0;
         }
-    	steps_total += steps;
+    	totalSteps += steps;
 
     	/*
 	 *	Reset count to 0 if it reaches 100
 	 */	
-    	if (steps_total > 99)
+    	if (totalSteps > 99)
 	{
-        	steps_total = 0;
+        	totalSteps = 0;
         }  
-    	printf("Total steps = %f\n", steps_total);
+    	printf("Total steps = %f\n", totalSteps);
 	return 0;
 }
