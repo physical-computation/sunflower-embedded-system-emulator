0211286b9b7c26f935c9c69fbf9737dcce159bef
JamesTimothyMeech <james.t.meech@gmail.com>
2020-11-03 15:32:35 +0000

    Finished with coding conventions

--- benchmarks/source/superh/Schlieren/virtualSensorExample.c
+++ benchmarks/source/superh/Schlieren/virtualSensorExample.c
@@ -24,16 +24,27 @@ Schlieren(float a, float b) {
 int
 main(void)
 {
+	/*
+	 *	Declare arrays to store sensor data and the size of said arrays
+	 */
 	const int 	samplesPerDistribution = 30; 
 	float 		temperatureDistribution[samplesPerDistribution];
 	float 		humidityDistribution[samplesPerDistribution];
+	/*
+	 *	Fill the arrays with sensor readings from Temperature.txt and Humidity.txt
+	 */
 	for(int i = 0; i < samplesPerDistribution; i++)
 	{
+		/*
+		 *	Delay by 30 us to match up with the sample rate of the sigsrc to avoid reading the same value twice or missing samples
+		 */
 		xudelay(30);
 		temperatureDistribution[i] = devsignal_read(0);
 		humidityDistribution[i] = devsignal_read(1);
 	}
-
+	/*
+	 *	Compute the sum divided by the difference for all pairs of sensor values and print the result
+	 */
 	for(int i = 0; i < samplesPerDistribution; i++)
 	{
 		printf("(%f + %f)/(%f - %f) = %f\n", temperatureDistribution[i], humidityDistribution[i], temperatureDistribution[i], humidityDistribution[i], Schlieren(temperatureDistribution[i], humidityDistribution[i]));
