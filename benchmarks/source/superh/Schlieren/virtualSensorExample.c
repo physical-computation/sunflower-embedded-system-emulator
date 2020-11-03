2fd3b98eb6ee8d8a023619df18e050475fb2b6b4
JamesTimothyMeech <james.t.meech@gmail.com>
2020-11-03 15:17:09 +0000

    Schlieren examplee working

---
+++ benchmarks/source/superh/Schlieren/virtualSensorExample.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+#include "sf-types.h"
+#include "tag.h"
+#include "devsim7708.h"
+#include "sh7708.h"
+#include "devscc.h"
+#include "devrtc.h"
+#include "devexcp.h"
+#include "devlog.h"
+#include "devloc.h"
+#include "devsensor.h"
+#include "misc.h"
+#include "print.h"
+
+float Schlieren(float a, float b) {
+	        return (a+b)/(a-b);
+}
+
+int
+main(void)
+{
+	const int samplesPerDistribution = 30; 
+	float temperatureDistribution[samplesPerDistribution];
+	float humidityDistribution[samplesPerDistribution];
+	float temperatureMean;
+	float humidityMean; 
+	float temperatureStandardDeviation;
+	float humidityStandardDeviation;
+	for(uint32_t i = 0; i < samplesPerDistribution; i++)
+	{
+		xudelay(30);
+		temperatureDistribution[i] = devsignal_read(0);
+		humidityDistribution[i] = devsignal_read(1);
+	}
+
+	for(uint32_t i = 0; i < samplesPerDistribution; i++)
+	{
+		printf("(%f + %f)/(%f - %f) = %f\n", temperatureDistribution[i], humidityDistribution[i], temperatureDistribution[i], humidityDistribution[i], Schlieren(temperatureDistribution[i], humidityDistribution[i]));
+	}	
+	return 0;
+}
