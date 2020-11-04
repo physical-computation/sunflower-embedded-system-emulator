1d1777e794ec7e5bda9a71db5caea7b2467e7ecd
JamesTimothyMeech <james.t.meech@gmail.com>
2020-11-04 14:28:18 +0000

    Applied coding conventions

--- benchmarks/source/superh/lowPassFilter/virtualSensorExample.c
+++ benchmarks/source/superh/lowPassFilter/virtualSensorExample.c
@@ -20,7 +20,12 @@
 #include "misc.h"
 #include "print.h"
 
-float LowPassFilter(float *in, float *out, int N, double in0)
+/* 
+ * From https://www.dsprelated.com/freebooks/filters/Simplest_Lowpass_Filter_I.html 
+ */
+
+float 
+LowPassFilter(float *in, float *out, int N, double in0)
 {
 	out[0] = in[0] + in0;
 	for (int n=1; n < N ; n++) 
@@ -33,11 +38,11 @@ float LowPassFilter(float *in, float *out, int N, double in0)
 int
 main(void)
 {
-	const int numberOfSamples = 30;
-	int halfNumberOfSamples = numberOfSamples/2;
-	float xBuffer[numberOfSamples];
-	float xBufferOut[numberOfSamples];
-	float in0 = 0;
+	const int	numberOfSamples = 30;
+	int		halfNumberOfSamples = numberOfSamples/2;
+	float 		xBuffer[numberOfSamples];
+	float 		xBufferOut[numberOfSamples];
+	float 		in0 = 0;
 	for(int j = 0; j < numberOfSamples; j++)
     	{
 		/* 
@@ -56,7 +61,7 @@ main(void)
 	   	 *	If we set xudelay to a higher value we will start to miss some samples in the file as we are looking at the return value of i
 		 *	devsignal_read() at a frequency lower than 400 Hz.
 	 	 */
-    	}
+	}	
 	
 	in0 = LowPassFilter(xBuffer, xBufferOut, halfNumberOfSamples, in0);
 	in0 = LowPassFilter(&xBuffer[halfNumberOfSamples], &xBufferOut[halfNumberOfSamples], halfNumberOfSamples, in0);
@@ -65,7 +70,6 @@ main(void)
 	{
 		printf("x[%d]=%f\ty[%d]=%f\n",i,xBuffer[i], i, xBufferOut[i]);
 	}	
-
 	return 0;
 }
 
